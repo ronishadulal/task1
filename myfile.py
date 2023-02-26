016fe41c1650b5fb892b96ed479e08a1471ecc69
ronishadulal <ronishadulal10@gmail.com>
2023-02-26 15:27:21 +0545

    update

---
+++ myfile.py
@@ -0,0 +1,34 @@
+w = 1.5
+h = 2.5
+print(w*h)
+print('Kate Austen')
+print('123 Full Circle Drive')
+print('Ashville, NC 28899')
+print("don't fear!")
+print("I'm here!")
+print('your assignment is read "Hamlet" by tomorrow.')
+print("""I'm reading "Hamlet" tonight.""")
+print("""One
+Two
+Three""")
+# This program displays a person's
+# name and address.
+print('Kate Austen')
+print('123 Full Circle Drive')
+print('Asheville, NC 28899')
+print('Kate Austen') # Display the name.
+print('123 Full Circle Drive') # Display the address.
+print('Asheville, NC 28899') # Display the city, state, and ZIP.
+a = 1
+b = 5
+c = -36
+x1 = (-b-((b*b)-(4*a*c))**0.5)/2*a
+x2= (-b+((b*b)-(4*a*c))**0.5)/2*a
+D= ((b*b)-(4*a*c))**0.5
+root1 = (b-D)/(2*a)
+root2 = (b+D)/(2*a)
+print(root1)
+print(root2)
+print(x1,x2)
+
+
